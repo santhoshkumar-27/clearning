8cc807a7a7cc1fc0832d5fba5d957bf355f5f0cc
santhoshkumar <its.santhoshkumar@outlook.com>
2023-08-05 19:47:18 +0530

    get input from the user

--- hello.c
+++ hello.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, world \n");
+int main(void)
+{
+    // Create an integer variable that will store the chars we get from the user
+    char answer[100];
+    // Ask the user to type a number
+    printf("Whats' your name: \n");
+
+    // Get and save the number the user types
+    scanf("%s", answer);
+
+    // Output the number the user typed
+    printf("hello,  %s \n", answer);
 }
