5615e8f481203b299c066c972af9f0e08e0a07e5
santhoshkumar <its.santhoshkumar@outlook.com>
2023-08-13 12:36:25 +0530

    discount is added

--- discounts.c
+++ discounts.c
@@ -1,9 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+float discount_price(float regular_price, float discount);
+
 void main(void)
 {
     float regular = get_float("Enter the regular: ");
-    float sale = regular * 0.85;
-    printf("your price is %.2f \n", sale);
+    printf("your price is %.2f \n", discount_price(regular, 25));
+}
+
+float discount_price(float regular_price, float discount)
+{
+    
+    return regular_price * ((100 - discount) / 100);
 }
