394da8f42537853ae5d74161c74b19600a3f72e2
santhoshkumar <its.santhoshkumar@outlook.com>
2023-08-16 07:09:58 +0530

    fix: scores

---
+++ scores.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void)
+{
+    int score1 = 72;
+
+    int score2 = 73;
+
+    int score3 = 74;
+
+    printf("average %.3f \n", (score1 + score2 + score3) / 3.0);
+}
