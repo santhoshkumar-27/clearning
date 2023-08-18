80d6ab17c274b6fe205f01da0a028e10096934bf
santhoshkumar <its.santhoshkumar@outlook.com>
2023-08-18 17:53:32 +0530

    fix: changes of scores

--- scores.c
+++ scores.c
@@ -3,13 +3,22 @@
 void main(void)
 {
     int scores[3];
-
-    scores[0] = get_int("Score  :");
-    scores[1] = get_int("Score  :");
-    scores[2] = get_int("Score  :");
+    int total = 0;
+    // scores[0] = get_int("Score  :");
+    // scores[1] = get_int("Score  :");
+    // scores[2] = get_int("Score  :");
 
     // variable to store size of Arr
     int length = sizeof(scores) / sizeof(scores[0]);
-    printf("length %i \n", length);
-    printf("average %.3f \n", (scores[9] + scores[1] + scores[2]) / 3.0);
+    for (int i = 0; i < length; i++)
+    {
+        do
+        {
+            scores[i] = get_int("Enter the %i score: ", i + 1);
+        } while (scores[i] < 0);
+
+        total += scores[i];
+    }
+    printf("length %f %i\n", (float)length, total);
+    printf("average %.3f \n", (total) / (float)length);
 }
