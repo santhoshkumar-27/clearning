238340cde0e6b2201d0b8756820376dfb8e17b30
santhoshkumar <its.santhoshkumar@outlook.com>
2023-08-23 22:20:27 +0530

    fix: phonebook search funtion and custom type implementations

--- phonebook.c
+++ phonebook.c
@@ -2,15 +2,32 @@
 #include <cs50.h>
 #include <strings.h>
 
+typedef struct
+{
+    string name;
+    string number;
+} person;
+
 int main(void)
 {
-    string names[] = {"santhosh", "kumar"};
-    string numbers[] = {"+91-6381130966", "+918682994321"};
+    person people[2];
+
+    people[0].name = "santhosh";
+    people[0].number = "+918682994321";
+
+    people[1].name = "kumar";
+    people[1].number = "+91-6381130966";
+
+    // string names[] = {"santhosh", "kumar"};
+    // string numbers[] = {"+91-6381130966", "+918682994321"};
+
+    printf("asdf %p \n", people);
+
     for (int i = 0; i < 2; i++)
     {
-        if (strcasecmp(names[i], "santhosh") == 0)
+        if (strcasecmp(people[i].name, "santhosh") == 0)
         {
-            printf("Found numbers is %s\n", numbers[i]);
+            printf("Found numbers is %s\n", people[i].number);
             return 0;
         }
     }
