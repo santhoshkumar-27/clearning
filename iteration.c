587b4b5178faa4b4afffd9aad27dc9645b890c2d
santhoshkumar <its.santhoshkumar@outlook.com>
2023-08-23 22:31:33 +0530

    iterations for printing the #

---
+++ iteration.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <strings.h>
+
+void draw(int h);
+
+int main(void)
+{
+    int height = get_int("Enter a value for, How tall you need?: ");
+    draw(height);
+    return 0;
+}
+
+void draw(int h)
+{
+    for (int i = 0; i <= h; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
