40518e5e63684560cc457ba5a05b9d3e5568d5b3
santhoshkumar <its.santhoshkumar@outlook.com>
2023-08-26 12:36:00 +0530

    fix: changes of array notations

--- stringsc.c
+++ stringsc.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
+    int numbers[] = {2, 3, 5, 6, 8};
     char *s = "hi!"; // this is the same like the string datatype but only stores the first element of memory address
     // string ss = "hi!";
     // char c = s[0]; // this will create a memory address
@@ -23,10 +24,19 @@ int main(void)
 
     // now we use square braceket to get the values with index 
     // we are diving into deep how they access;
+
     // pointer arthmetic
-    printf("%c \n", *s); // --> s[0]
-    printf("%c \n", *(s + 1)); // --> s[1]
-    printf("%c \n", *(s + 2)); // --> s[2]
 
+    // printf("%c \n", *s); // --> s[0]
+    // printf("%c \n", *(s + 1)); // --> s[1]
+    // printf("%c \n", *(s + 2)); // --> s[2]
 
+    printf("%i \n", numbers[0]);
+    printf("%i \n", numbers[1]);
+    printf("%i \n", numbers[2]);
+    // both are same but above one use square bracket notations to access value
+    // below one using address to get the values 
+    printf("%i \n", *numbers);
+    printf("%i \n", *(numbers + 1));
+    printf("%i \n", *(numbers + 2));
 }
